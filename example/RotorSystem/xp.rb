fc09865904ad074dadc55b04879c2e45d824b96f
Ryan Goodfellow <rgoodfel@isi.edu>
2015-09-27 22:25:14 -0700

    cleaning

--- example/RotorSystem/xp.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-controller = "/cys/build/control/lib/RotorSpeedController"
-sim = "/cys/build/example/RotorSystem/sim/RotorSystem"
-xpdir = "/cys/example/RotorSystem/control"
-
-["rca", "rcb", "rcc"].each { |c| xp.nodes.push(controller, ["#{xpdir}/#{c}.yaml"]) }
-xp.nodes.push(sim, [])
-
