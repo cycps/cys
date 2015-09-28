0804e32df56ff5a2c44c5d081bf95637418fc150
Ryan Goodfellow <rgoodfel@isi.edu>
2015-09-28 10:43:54 -0700

    reticulating splines

--- xp.go
+++ xp.go
@@ -2,6 +2,7 @@ package xp
 
 import (
 	"fmt"
+	"os"
 )
 
 type Experiment struct {
@@ -19,8 +20,26 @@ func NewNode(name, cmd string, args ...string) {
 	XP.Nodes = append(XP.Nodes, Node{name, cmd, args})
 }
 
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: xp [up | down | run]\n")
+	os.Exit(1)
+}
+
 func Main() {
 
-	fmt.Println("apple muffins")
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "up":
+		fmt.Println("bringing environment up")
+	case "down":
+		fmt.Println("bringing environment down")
+	case "run":
+		fmt.Println("running experiment")
+	default:
+		usage()
+	}
 
 }
