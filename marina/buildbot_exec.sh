59813800823e202ceb2fbc55ae055b87d3cda49a
Ryan Goodfellow <rgoodfel@isi.edu>
2015-09-28 16:35:54 -0700

    reticulating splines

---
+++ marina/buildbot_exec.sh
@@ -0,0 +1,3 @@
+#/usr/bin/env bash
+
+docker exec -it buildbot bash
--- marina/docker19_vb_install.sh
+++ marina/docker19_vb_install.sh
@@ -1,5 +1,8 @@
 #/usr/bin/env bash
 
-docker-machine create -d virtualbox \
-  --virtualbox-boot2docker-url=http://sirile.github.io/files/boot2docker-1.9.iso \
-  buildbot
+docker-machine status marina
+if [ $? -eq 0]; then
+  docker-machine create -d virtualbox \
+    --virtualbox-boot2docker-url=http://sirile.github.io/files/boot2docker-1.9.iso \
+    buildbot
+fi
