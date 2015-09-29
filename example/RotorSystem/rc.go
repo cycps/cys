a22221a6260e80ed2aef53bb6fb5169b2b03aad8
Ryan Goodfellow <rgoodfel@isi.edu>
2015-09-28 21:44:48 -0700

    reticulating splines

--- example/RotorSystem/rc.go
+++ example/RotorSystem/rc.go
@@ -2,23 +2,25 @@ package main
 
 import "github.com/cycps/cys"
 
+const (
+	basedir     = "/Users/ry/@/cycps/cys"
+	rsc         = basedir + "/build/control/lib/RotorSpeedController"
+	sim         = basedir + "/build/example/RotorSystem/sim/RotorSystem"
+	simsettings = basedir + "/build/example/RotorSystem/sim/RotorSystem"
+	xpdir       = basedir + "/example/RotorSystem/control"
+)
+
 func main() {
 	xp.Name("rc")
-	rsc := "/Users/ry/@/cycps/cys/build/control/lib/RotorSpeedController"
-	sim := "/Users/ry/@/cycps/cys/build/example/RotorSystem/sim/RotorSystem"
-	xpdir := "/Users/ry/@/cycps/cys/example/RotorSystem/control"
-	rscs := []string{"rca", "rcb", "rcc"}
+	controllers := []string{"rca", "rcb", "rcc"}
 
-	for _, c := range rscs {
-
+	for _, c := range controllers {
 		cfg := c + ".yaml"
 		fm := xp.FileMap{xpdir + "/" + cfg, "/cyp/" + cfg}
-
-		xp.NewNode(c, rsc, fm.Remote).
-			AddFileMap(fm)
-
+		xp.NewController(c, rsc, fm.Remote).AddFileMap(fm)
 	}
-	xp.NewNode("sim", sim)
+
+	xp.SetSim(sim, xpdir+"/sim.yaml")
 
 	xp.Main()
 }
--- xp.go
+++ xp.go
@@ -18,6 +18,7 @@ func Main() {
 		up()
 	case "down":
 		fmt.Println("bringing environment down")
+		down()
 	case "run":
 		fmt.Println("running experiment")
 	default:
@@ -27,8 +28,9 @@ func Main() {
 }
 
 type Experiment struct {
-	Name  string
-	Nodes []*Node
+	Name        string
+	Controllers []*Node
+	Sim         *Node
 }
 
 type FileMap struct {
@@ -43,13 +45,23 @@ type Node struct {
 
 var XP = new(Experiment)
 
-func NewNode(name, exe string, args ...string) *Node {
+func NewController(name, exe string, args ...string) *Node {
 	n := new(Node)
 	n.Files = make(map[string]string)
 	n.Name = name
 	n.Exe = exe
 	n.Args = args
-	XP.Nodes = append(XP.Nodes, n)
+	XP.Controllers = append(XP.Controllers, n)
+	return n
+}
+
+func SetSim(exe string, settings string) *Node {
+	n := new(Node)
+	n.Files = make(map[string]string)
+	n.Name = "sim"
+	n.Exe = exe
+	n.Args = []string{settings}
+	XP.Sim = n
 	return n
 }
 
@@ -74,6 +86,59 @@ func cmdErr(err error, msg string, out []byte) {
 	os.Exit(1)
 }
 
+func initNode(n *Node) {
+	netname := XP.Name + "net"
+
+	out, err := exec.Command(
+		"docker", "service", "publish", n.Name+"."+netname).CombinedOutput()
+	if err != nil {
+		cmdErr(err, "Publishing service for "+n.Name+"failed", out)
+	}
+
+	out, err = exec.Command(
+		"docker", "run", "-itd", "--hostname="+n.Name, "--name="+n.Name,
+		"-v", "/cys:/cys", "cycps/cys").Output()
+	if err != nil {
+		cmdErr(err, "Running container for "+n.Name+" failed", out)
+	}
+
+	out, err = exec.Command(
+		"docker", "service", "attach", n.Name, n.Name+"."+netname).CombinedOutput()
+	if err != nil {
+		cmdErr(err, "Attaching "+n.Name+" container to the network failed", out)
+	}
+
+	out, err = exec.Command(
+		"docker", "exec", n.Name, "mkdir", "-p", "/app").CombinedOutput()
+	if err != nil {
+		cmdErr(err, "Making /app dir failed on node "+n.Name, out)
+	}
+	out, err = exec.Command(
+		"docker", "exec", n.Name, "mkdir", "-p", "/cyp").CombinedOutput()
+	if err != nil {
+		cmdErr(err, "Making /cyp dir failed on node "+n.Name, out)
+	}
+	out, err = exec.Command(
+		"docker", "cp", n.Exe, n.Name+":/app/").CombinedOutput()
+	if err != nil {
+		cmdErr(err, "Copying executable to "+n.Name+" failed", out)
+	}
+
+	for local, remote := range n.Files {
+
+		out, err = exec.Command(
+			"docker", "cp", local, n.Name+":"+remote).CombinedOutput()
+		if err != nil {
+			cmdErr(err,
+				fmt.Sprintf("Copying local file %s to remote destination %s failed",
+					local, remote),
+				out)
+		}
+
+	}
+
+}
+
 func up() {
 
 	netname := XP.Name + "net"
@@ -84,52 +149,46 @@ func up() {
 		cmdErr(err, "Creating docker network "+netname+" failed", out)
 	}
 
-	for _, n := range XP.Nodes {
-		out, err = exec.Command(
-			"docker", "service", "publish", n.Name+"."+netname).CombinedOutput()
-		if err != nil {
-			cmdErr(err, "Publishing service for "+n.Name+"failed", out)
-		}
+	for _, n := range XP.Controllers {
+		initNode(n)
+	}
 
-		out, err = exec.Command(
-			"docker", "run", "-itd", "--hostname="+n.Name, "--name="+n.Name,
-			"-v", "/cys:/cys", "cycps/cys").Output()
-		if err != nil {
-			cmdErr(err, "Running container for "+n.Name+" failed", out)
-		}
+	initNode(XP.Sim)
 
-		out, err = exec.Command(
-			"docker", "service", "attach", n.Name, n.Name+"."+netname).CombinedOutput()
-		if err != nil {
-			cmdErr(err, "Attaching "+n.Name+" container to the network failed", out)
-		}
+}
 
-		out, err = exec.Command(
-			"docker", "exec", n.Name, "mkdir", "-p", "/app").CombinedOutput()
-		if err != nil {
-			cmdErr(err, "Making /app dir failed on node "+n.Name, out)
-		}
-		out, err = exec.Command(
-			"docker", "exec", n.Name, "mkdir", "-p", "/cyp").CombinedOutput()
-		if err != nil {
-			cmdErr(err, "Making /cyp dir failed on node "+n.Name, out)
-		}
-		out, err = exec.Command(
-			"docker", "cp", n.Exe, n.Name+":/app/").CombinedOutput()
-		if err != nil {
-			cmdErr(err, "Copying executable to "+n.Name+" failed", out)
-		}
+func shutdownNode(n *Node) {
 
-		for local, remote := range n.Files {
+	out, err := exec.Command("docker", "stop", "-t", "0", n.Name).CombinedOutput()
+	if err != nil {
+		cmdErr(err, "Error shutting down node "+n.Name, out)
+	}
 
-			out, err = exec.Command(
-				"docker", "cp", local, n.Name+":"+remote).CombinedOutput()
-			if err != nil {
-				cmdErr(err, fmt.Sprintf("Copying local file %s to remote destination %s failed",
-					local, remote), out)
-			}
+	out, err = exec.Command("docker", "rm", n.Name).CombinedOutput()
+	if err != nil {
+		cmdErr(err, "Error removing node "+n.Name, out)
+	}
 
-		}
+	sname := n.Name + "." + XP.Name + "net"
+	out, err = exec.Command("docker", "service", "unpublish", sname).CombinedOutput()
+	if err != nil {
+		cmdErr(err, "Error unpublishing service "+sname, out)
+	}
+
+}
+
+func down() {
+
+	for _, n := range XP.Controllers {
+		shutdownNode(n)
+	}
+
+	shutdownNode(XP.Sim)
+
+	netname := XP.Name + "net"
+	out, err := exec.Command("docker", "network", "rm", netname).CombinedOutput()
+	if err != nil {
+		cmdErr(err, "Error removing network "+netname, out)
 	}
 
 }
